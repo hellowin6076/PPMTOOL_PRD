44dc0a8b4d719aa5cb4e8d42eb07fa1b34951928
lee.huimok <hellowin6076@gmail.com>
2021-08-18 23:10:27 +0900

    firtst release

---
+++ src/main/java/io/bufgix/ppmtool/domain/Backlog.java
@@ -0,0 +1,68 @@
+package io.bufgix.ppmtool.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Backlog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSequence = 0;
+    private String projectIdentifier;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "projcet_id", nullable = false)
+    @JsonIgnore
+    private Project project;
+
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "backlog", orphanRemoval = true)
+    private List<ProjectTask> projectTask = new ArrayList<>();
+
+    public Backlog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPTSequence() {
+        return PTSequence;
+    }
+
+    public void setPTSequence(Integer PTSequence) {
+        this.PTSequence = PTSequence;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<ProjectTask> getProjectTask() {
+        return projectTask;
+    }
+
+    public void setProjectTask(List<ProjectTask> projectTask) {
+        this.projectTask = projectTask;
+    }
+}
---
+++ src/main/java/io/bufgix/ppmtool/services/ProjectService.java
@@ -0,0 +1,89 @@
+package io.bufgix.ppmtool.services;
+
+import io.bufgix.ppmtool.domain.Backlog;
+import io.bufgix.ppmtool.domain.Project;
+import io.bufgix.ppmtool.domain.User;
+import io.bufgix.ppmtool.exceptions.ProjectIdException;
+import io.bufgix.ppmtool.exceptions.ProjectNotFoundException;
+import io.bufgix.ppmtool.repositories.BacklogRepository;
+import io.bufgix.ppmtool.repositories.ProjectRepository;
+import io.bufgix.ppmtool.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public Project saveOrUpdateProject(Project project, String username){
+        String projectIdentifier = project.getProjectIdentifier().toUpperCase();
+        // project.getId != null
+        // find by db -> null
+
+        if(project.getId() != null){
+            Project existingProject = projectRepository.findByProjectIdentifier(projectIdentifier);
+
+            if(existingProject != null && (!existingProject.getProjectLeader().equals(username))){
+                throw new ProjectNotFoundException("Project not found in your account");
+            }else if (existingProject == null){
+                throw new ProjectNotFoundException("Project with ID : '"+project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+            }
+        }
+
+        try{
+
+        	User user = userRepository.findByUsername(username);
+        	project.setUser(user);
+        	project.setProjectLeader(user.getUsername());
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId()==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId()!=null){
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+
+            return projectRepository.save(project);
+        }catch (Exception e) {
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectId, String username) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project Id '"+projectId+"' does not exist");
+        }
+        
+        if(!project.getProjectLeader().equals(username)) {
+        	throw new ProjectNotFoundException("Project not found in your account");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(String username){
+        return projectRepository.findAllByProjectLeader(username);
+    }
+
+    public void deleteProjectByIdentifier(String projectId, String username){
+
+        projectRepository.delete(findProjectByIdentifier(projectId, username));
+    }
+}
