37d97993f34b6d248b5aaff610a66ca07568ce35
lee <hellowin6076@naver.com>
2021-08-19 10:36:30 +0900

    no message

--- src/main/java/io/bufgix/ppmtool/PpmtoolApplication.java
+++ src/main/java/io/bufgix/ppmtool/PpmtoolApplication.java
@@ -2,10 +2,13 @@ package io.bufgix.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ComponentScan
 public class PpmtoolApplication {
 
 	@Bean
